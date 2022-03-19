3be8de270da954239d6c741fe56787091aef80f7
esysolina <evelina.ova@gmail.com>
2022-03-19 19:31:38 +0500

    added extends

--- src/test/java/guru/qa/NotifyTest.java
+++ src/test/java/guru/qa/NotifyTest.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
 
-public class NotifyTest extends guru.qa.tests.TestBase {
+public class NotifyTest extends guru.qa.TestBase {
 
     @Test
     void selenideTest () {
